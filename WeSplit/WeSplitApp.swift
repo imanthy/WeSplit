21e3b62ef085f99dc0d0466c93e5f769ac3108e0
Anthy Chen <koyun.chen@gmail.com>
2023-03-02 14:47:20 -0800

    Initial commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Anthy Chen on 3/2/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
